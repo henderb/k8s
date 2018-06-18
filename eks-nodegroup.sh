54e407bbf83de4c83f594df66d549c7308ec2aae
Brian Henderson <theinitialsarebh@gmail.com>
2018-06-18 13:34:19 -0700

    it might be working

---
+++ eks-nodegroup.sh
@@ -0,0 +1,45 @@
+CLUSTER_NAME=${CLUSTER_NAME:-henderb}
+SECURITY_GROUP=${SECURITY_GROUP:-sg-a71864d6}
+VPC=${VPC:-vpc-ffe58c9a}
+SSH_KEYPAIR=${SSH_KEYPAIR:-henderb}
+
+SUBNETS="subnet-1e841c69 subnet-2c7f0549 subnet-f968d5a0"
+NODE_SIZES="m5.large c5.large r4.large"
+
+CF_TEMPLATE=https://henderb-cf-templates.s3-us-west-2.amazonaws.com/amazon-eks-nodegroup-per-group.yaml
+NODE_AMI=ami-73a6e20b
+
+NODE_INSTANCE_PROFILE="arn:aws:iam::157842721751:instance-profile/henderb-eks-nodes-core-NodeInstanceProfile-YJJTLVDD2TJ7"
+NODE_INSTANCE_ROLE="arn:aws:iam::157842721751:role/henderb-eks-nodes"
+NODE_SECURITY_GROUP="sg-7aa38e0b"
+
+export AWS_PROFILE=brian
+
+#############################
+
+set -e
+#set -x
+
+for SUBNET in $SUBNETS; do
+    for NODE_SIZE in $NODE_SIZES; do
+        NODE_SIZE_NAME=$(echo ${NODE_SIZE} | sed 's/\./-/')
+        aws cloudformation create-stack \
+            --stack-name ${CLUSTER_NAME}-${SUBNET}-${NODE_SIZE_NAME}-nodes \
+            --template-url ${CF_TEMPLATE} \
+            --capabilities CAPABILITY_NAMED_IAM \
+            --parameters \
+                ParameterKey=ClusterControlPlaneSecurityGroup,ParameterValue=${SECURITY_GROUP} \
+                ParameterKey=ClusterName,ParameterValue=${CLUSTER_NAME} \
+                ParameterKey=KeyName,ParameterValue=${SSH_KEYPAIR} \
+                ParameterKey=NodeAutoScalingGroupMaxSize,ParameterValue=0 \
+                ParameterKey=NodeAutoScalingGroupMinSize,ParameterValue=0 \
+                ParameterKey=NodeGroupName,ParameterValue=${CLUSTER_NAME}-${SUBNET}-${NODE_SIZE_NAME}-nodes \
+                ParameterKey=NodeImageId,ParameterValue=${NODE_AMI} \
+                ParameterKey=NodeInstanceType,ParameterValue=${NODE_SIZE} \
+                ParameterKey=NodeInstanceProfile,ParameterValue=${NODE_INSTANCE_PROFILE} \
+                ParameterKey=NodeSecurityGroup,ParameterValue=${NODE_SECURITY_GROUP} \
+                ParameterKey=VpcId,ParameterValue=${VPC} \
+                ParameterKey=Subnets,ParameterValue=${SUBNET}
+    done
+done
+
--- eks-up.sh
+++ eks-up.sh
@@ -1,15 +1,17 @@
 CLUSTER_NAME=${CLUSTER_NAME:-henderb}
 SUBNETS=${SUBNETS:-subnet-1e841c69,subnet-2c7f0549,subnet-f968d5a0}
-SECURITY_GROUP=${SECURITY_GROUP:-sg-0a87eb7b}
+MASTERS_SECURITY_GROUP=${SECURITY_GROUP:-sg-a71864d6}
 VPC=${VPC:-vpc-ffe58c9a}
 SSH_KEYPAIR=${SSH_KEYPAIR:-henderb}
+
+CF_TEMPLATE=https://henderb-cf-templates.s3-us-west-2.amazonaws.com/amazon-eks-nodegroup-core.yaml
 
 export AWS_PROFILE=brian
 
 #############################
 
 set -e
-#set -x
+set -x
 
 CLUSTER_STATUS=$(aws eks describe-cluster --name ${CLUSTER_NAME} --query cluster.status | tr -d \")
 until [ "${CLUSTER_STATUS}" == "ACTIVE" ]; do
@@ -21,23 +23,16 @@ until [ "${CLUSTER_STATUS}" == "ACTIVE" ]; do
         aws eks create-cluster \
             --name ${CLUSTER_NAME} \
             --role-arn arn:aws:iam::157842721751:role/aws-eks \
-            --resources-vpc-config subnetIds=${SUBNETS},securityGroupIds=${SECURITY_GROUP}
+            --resources-vpc-config subnetIds=${SUBNETS},securityGroupIds=${MASTERS_SECURITY_GROUP}
 
         aws cloudformation create-stack \
-            --stack-name ${CLUSTER_NAME}-worker-nodes \
-            --template-url https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-06-05/amazon-eks-nodegroup.yaml \
-            --capabilities CAPABILITY_IAM \
+            --stack-name ${CLUSTER_NAME}-eks-nodes-core \
+            --template-url ${CF_TEMPLATE} \
+            --capabilities CAPABILITY_NAMED_IAM \
             --parameters \
-                ParameterKey=ClusterControlPlaneSecurityGroup,ParameterValue=${SECURITY_GROUP} \
+                ParameterKey=ClusterControlPlaneSecurityGroup,ParameterValue=${MASTERS_SECURITY_GROUP} \
                 ParameterKey=ClusterName,ParameterValue=${CLUSTER_NAME} \
-                ParameterKey=KeyName,ParameterValue=${SSH_KEYPAIR} \
-                ParameterKey=NodeAutoScalingGroupMaxSize,ParameterValue=3 \
-                ParameterKey=NodeAutoScalingGroupMinSize,ParameterValue=1 \
-                ParameterKey=NodeGroupName,ParameterValue=${CLUSTER_NAME}-worker-nodes \
-                ParameterKey=NodeImageId,ParameterValue=ami-73a6e20b \
-                ParameterKey=NodeInstanceType,ParameterValue=t2.medium \
-                ParameterKey=VpcId,ParameterValue=${VPC} \
-                ParameterKey=Subnets,ParameterValue=\"${SUBNETS}\"
+                ParameterKey=VpcId,ParameterValue=${VPC}
     fi
     CLUSTER_STATUS=$(aws eks describe-cluster --name ${CLUSTER_NAME} --query cluster.status | tr -d \")
 done
@@ -60,20 +55,5 @@ kubectl config set-context ${CLUSTER_NAME} \
 
 kubectl config use-context ${CLUSTER_NAME}
 
-NODE_INSTANCE_ROLE=$(aws cloudformation describe-stacks --stack-name ${CLUSTER_NAME}-worker-nodes --query 'Stacks[0].Outputs[?OutputKey==`NodeInstanceRole`].OutputValue' --output text)
-cat >eks-node-cm.yaml <<EOF
-apiVersion: v1
-kind: ConfigMap
-metadata:
-  name: aws-auth
-  namespace: kube-system
-data:
-  mapRoles: |
-    - rolearn: ${NODE_INSTANCE_ROLE}
-      username: system:node:{{EC2PrivateDNSName}}
-      groups:
-        - system:bootstrappers
-        - system:nodes
-EOF
 kubectl apply -f eks-node-cm.yaml
 
